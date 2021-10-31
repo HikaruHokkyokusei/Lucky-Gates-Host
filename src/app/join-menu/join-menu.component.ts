26cc83425b7f12738b91d1f8eec22416672937b6
HentaiOusama <kutabare.eerito@gmail.com>
2021-10-31 15:15:37 +0530

    Updated Player Menu. Added Join Menu.

--- src/app/join-menu/join-menu.component.ts
+++ src/app/join-menu/join-menu.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {AppComponent} from "../app.component";
 
 @Component({
-  selector: 'app-join-menu',
+  selector: 'app-join-menu[appComponent]',
   templateUrl: './join-menu.component.html',
   styleUrls: ['./join-menu.component.css']
 })
 export class JoinMenuComponent implements OnInit {
 
+  @Input() appComponent!: AppComponent;
+
   constructor() { }
 
   ngOnInit(): void {
   }
-
 }
