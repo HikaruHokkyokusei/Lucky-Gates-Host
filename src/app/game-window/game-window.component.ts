92de4ddf8f2b5913a0d59a7de8d87989cc295eaa
HentaiOusama <kutabare.eerito@gmail.com>
2021-11-03 17:56:42 +0530

    Prepared Base of Game Window

--- src/app/game-window/game-window.component.ts
+++ src/app/game-window/game-window.component.ts
@@ -9,6 +9,7 @@ import {AppComponent} from "../app.component";
 export class GameWindowComponent implements OnInit {
 
   @Input() appComponent!: AppComponent;
+  doorIndices: number[] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
   constructor() {
   }
