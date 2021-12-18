12c8078e7df26ba78c9dd8b85274c28ffca391c5
Hentai Ousama <kutabare.eerito@gmail.com>
2021-12-18 19:13:16 +0530

    Prepared Base for more information on game window.

--- src/app/UIElements/button/button.component.ts
+++ src/app/UIElements/button/button.component.ts
@@ -1,4 +1,5 @@
-import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {AfterViewInit, Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import * as uuid from "uuid";
 import {ThemeService} from "../../theme.service";
 
 @Component({
@@ -6,14 +7,16 @@ import {ThemeService} from "../../theme.service";
   templateUrl: './button.component.html',
   styleUrls: ['./button.component.css']
 })
-export class ButtonComponent implements OnInit {
+export class ButtonComponent implements OnInit, AfterViewInit {
   @Input() buttonText: string = "";
   @Input() millisBetweenClicks: number = 2500;
   @Output() onButtonClick: EventEmitter<any> = new EventEmitter();
 
+  id: string = uuid.v4();
+  buttonBg: HTMLElement | null = null;
   nextClickTime: number = Date.now();
   textColor: string;
-  buttonBgColor: string; // TODO : Bind this var in the html file
+  buttonBgColor: string;
 
   constructor() {
     let theme = ThemeService.getTheme();
@@ -27,10 +30,28 @@ export class ButtonComponent implements OnInit {
     }
   }
 
+  ngAfterViewInit() {
+    this.buttonBg = document.getElementById(this.id);
+  }
+
   onClick() {
     if (Date.now() >= this.nextClickTime) {
       this.onButtonClick.emit();
       this.nextClickTime = Date.now() + this.millisBetweenClicks;
+    }
+  }
+
+  shrink() {
+    if (this.buttonBg != null) {
+      this.buttonBg.style.opacity = "0";
+      this.buttonBg.style.transform = "scale(0.4, 0.4)";
+    }
+  }
+
+  expand() {
+    if (this.buttonBg != null) {
+      this.buttonBg.style.opacity = "1";
+      this.buttonBg.style.transform = "scale(1, 1)";
     }
   }
 }
